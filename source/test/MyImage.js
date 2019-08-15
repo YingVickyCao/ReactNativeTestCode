79536de464321338ac66ced5c18a091de606cef5
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 20:07:28 +0800

    RN:Image View

--- source/test/MyImage.js
+++ source/test/MyImage.js
@@ -1,11 +1,13 @@
-import React, { Compoent } from 'react'
-import { Image } from 'react-native'
+import React, { Component } from 'react';
+import { Image } from 'react-native';
 
-export default class MyImage extends Compoent {
-    render() {
-        let pic = {
-            uri: 'https://www.baidu.com/img/bd_logo1.png'
-          };
-        return <Image source={pic} style={{ width: 100, height: 100 }}/>
-    }
+export default class MyImage extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}} />
+    );
+  }
 }
