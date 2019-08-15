2158fd3385ed3b1a07bac8336445b6594576f913
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 20:22:16 +0800

    RN: custeom view

--- source/test/index.js
+++ source/test/index.js
@@ -5,8 +5,8 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import App from './MyText';
-
-import App from './MyImage';
+// import App from './MyImage';
+import App from './CustomView';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
