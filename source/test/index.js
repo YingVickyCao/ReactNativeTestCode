80d2a3be19eb41f4c6607323dc75f4cffdf13e75
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 20:56:12 +0800

    refactor

--- source/test/index.js
+++ source/test/index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 // import App from './MyText';
 // import App from './MyImage';
 // import App from './CustomView';
-import App from './BlinkView';
+import App from './TestStateView';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
