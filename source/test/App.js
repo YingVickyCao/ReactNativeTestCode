7890e47e7780039e03ab791a8fa0d3c3b3095cb8
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 19:48:08 +0800

    my text

--- source/test/App.js
+++ source/test/App.js
@@ -29,12 +29,13 @@ export default class App extends Component<Props> {
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FF0000',
   },
   welcome: {
     fontSize: 20,
---
+++ source/test/MyText.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {Text,View}  from 'react-native'
+
+export default class Sample1 extends Component{
+    render(){
+        return (
+            <View  style={{flex:1, justifyContent:"center",alignItems:"center"}}>
+                <Text> A B C</Text>
+            </View>
+        );
+
+    }
+}
