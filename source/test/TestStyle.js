0ae7417334a0ce0f24c7b5e12cd19ab2453065fe
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 21:18:57 +0800

    Update TestStyle.js

--- source/test/TestStyle.js
+++ source/test/TestStyle.js
@@ -4,32 +4,33 @@ import { Text, View, StyleSheet, AppRegistry } from 'react-native'
 export default class Sample1 extends Component {
     render() {
         return (
-            <View style={{backgroundColor:'black'}}>
-                <Text style={{
-                    backgroundColor: 'green',
-                    color: 'red',
-                    fontWeight: 'bold',
-                    fontSize: 30,
-                    alignSelf: "center",
-                }}> Test Style</Text>
-            </View>
-
             // <View >
-            //     <Text style={styles.green}> Test Style </Text>
+            //     <Text style={{
+            //         color: '#00ff00',
+            //         fontWeight: 'bold',
+            //         fontSize: 30,
+            //         alignSelf: "center",
+            //     }}> Test Style</Text>
             // </View>
+
+            <View>
+                <Text> Test Style </Text>
+                <Text style={styles.green}> Test Style </Text>
+                <Text style={[styles.green, styles.red]}>Test Style</Text>
+                <Text style={[styles.red, styles.green]}>Test Style</Text>
+            </View>
         );
     }
 }
 const styles = StyleSheet.create({
     green: {
-        backgroundColor: 'green',
-        color: 'red',
+        color: '#00ff00',
         fontWeight: 'bold',
         fontSize: 30,
         alignSelf: "center",
     },
 
     red: {
-        color: 'red',
+        color: '#ff0000',
     },
 });
