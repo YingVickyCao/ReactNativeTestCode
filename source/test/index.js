d35072d1332e68f77b610465ab2515fb7a8af2a6
Vicky <ying.vicky.cao@outlook.com>
2019-08-16 16:22:55 +0800

    Update index.js

--- source/test/index.js
+++ source/test/index.js
@@ -8,11 +8,11 @@ import {AppRegistry} from 'react-native';
 // import App from './TestProps';
 // import App from './CustomView';
 // import App from './TestStateView';
-// import App from './TestHeightWidth';
+import App from './TestHeightWidth';
 // import App from './TestFlexDirection';
 // import App from './TestJustifyContent';
 // import App from './TestAlignItems';
-import App from './TestAlignSelf';
+// import App from './TestAlignSelf';
 
 import {name as appName} from './app.json';
 
