363a329afb7c146f452c9dcd9b297ae1696d83c6
Vicky <ying.vicky.cao@outlook.com>
2019-08-16 13:57:17 +0800

    RN: flexDirection

--- source/test/index.js
+++ source/test/index.js
@@ -4,10 +4,12 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './TestStyle';
+// import App from './TestStyle';
 // import App from './TestProps';
 // import App from './CustomView';
 // import App from './TestStateView';
+import App from './TestHeightWidth';
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
