f03268512f6585e77583132601d2cb9455782e25
Vicky <ying.vicky.cao@outlook.com>
2019-08-16 14:03:59 +0800

    refactor

--- source/test/index.js
+++ source/test/index.js
@@ -8,7 +8,8 @@ import {AppRegistry} from 'react-native';
 // import App from './TestProps';
 // import App from './CustomView';
 // import App from './TestStateView';
-import App from './TestHeightWidth';
+// import App from './TestHeightWidth';
+import App from './TestFlexDirection';
 
 import {name as appName} from './app.json';
 
