c7bcf18806db2212d8bfc6bf5cb1f0dd38c5629d
Vicky <ying.vicky.cao@outlook.com>
2019-08-22 21:08:57 +0800

    RN: ScrollView

--- source/test/index.js
+++ source/test/index.js
@@ -20,9 +20,10 @@ import {AppRegistry} from 'react-native';
 // import App from './communication_between_components/child_2_parent/ParentView';
 // import App from './network/TestImage'
 
-import App from './network/async/Movies'
+// import App from './network/async/Movies'
 // import App from './myview/TestList'
 // import App from './myview/TestSectionList'
+import App from './myview/TestScrollView'
 
 import {name as appName} from './app.json';
 
