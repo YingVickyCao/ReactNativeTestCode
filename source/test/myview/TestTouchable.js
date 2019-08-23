799f7953217fdfcc343b1062cf12ac1030a63001
Vicky <ying.vicky.cao@outlook.com>
2019-08-23 20:43:36 +0800

    Update TestTouchable.js

--- source/test/myview/TestTouchable.js
+++ source/test/myview/TestTouchable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Alert, Text, View, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, Platform, Alert, Text, View, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
 
 export default class TestTouchableViews extends Component {
     _onPressBtn() {
@@ -7,36 +7,56 @@ export default class TestTouchableViews extends Component {
     }
 
     render() {
-        return (
-            <View style={styles.container}>
-                <TouchableHighlight style={styles.btn} onPress={this._onPressBtn} underlayColor='#EB984E'>
-                    <Text style={styles.btnText}> Highlight </Text>
-                </TouchableHighlight>
+        if (Platform.OS === "ios") {
+            return (
+                <View style={styles.container}>
+                    <TouchableHighlight style={styles.btn} onPress={this._onPressBtn} underlayColor='#EB984E'>
+                        <Text style={styles.btnText}> Highlight </Text>
+                    </TouchableHighlight>
 
-                
-                {/* <TouchableNativeFeedback onPress={this._onPressBtn} background={TouchableNativeFeedback.SelectableBackgroundBorderless()}> */}
-                <TouchableNativeFeedback onPress={this._onPressBtn} background={TouchableNativeFeedback.SelectableBackground()}>
-                    <View style={[styles.btn, { backgroundColor: 'red' }]}>
-                        <Text> Feedback </Text>
-                    </View>
-                </TouchableNativeFeedback>
+                    <TouchableOpacity style={styles.btn} onPress={this._onPressBtn}>
+                        <Text style={styles.btnText}> Opatity </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity style={styles.btn} onPress={this._onPressBtn}>
-                    <Text style={styles.btnText}> Opatity </Text>
-                </TouchableOpacity>
+                    <TouchableWithoutFeedback style={styles.btn} onPress={this._onPressBtn}>
+                        <View style={[styles.btn, { backgroundColor: 'red' }]}>
+                            <Text style={styles.btnText}> Feedback </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View >
+            )
+        }
+        else {
+            return (
+                <View style={styles.container}>
+                    <TouchableHighlight style={styles.btn} onPress={this._onPressBtn} underlayColor='#EB984E'>
+                        <Text style={styles.btnText}> Highlight </Text>
+                    </TouchableHighlight>
 
-                <TouchableWithoutFeedback style={styles.btn} onPress={this._onPressBtn}>
-                    <View style={[styles.btn, { backgroundColor: 'red' }]}>
-                        <Text style={styles.btnText}> Feedback </Text>
-                    </View>
-                </TouchableWithoutFeedback>
-            </View >
-        )
+                    {/* <TouchableNativeFeedback onPress={this._onPressBtn} background={TouchableNativeFeedback.SelectableBackgroundBorderless()}> */}
+                    <TouchableNativeFeedback onPress={this._onPressBtn} background={TouchableNativeFeedback.SelectableBackground()}>
+                        <View style={[styles.btn, { backgroundColor: 'red' }]}>
+                            <Text> Feedback </Text>
+                        </View>
+                    </TouchableNativeFeedback>
+
+                    <TouchableOpacity style={styles.btn} onPress={this._onPressBtn}>
+                        <Text style={styles.btnText}> Opatity </Text>
+                    </TouchableOpacity>
+
+                    <TouchableWithoutFeedback style={styles.btn} onPress={this._onPressBtn}>
+                        <View style={[styles.btn, { backgroundColor: 'red' }]}>
+                            <Text style={styles.btnText}> Feedback </Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View >
+            )
+        }
     }
 }
 
 /**
- * TouchableNativeFeedback: Android only
+ * TouchableNativeFeedback: Android only.
  * TouchableWithoutFeedback: Depressed
  */
 const styles = StyleSheet.create({
@@ -51,13 +71,20 @@ const styles = StyleSheet.create({
     btn: {
         justifyContent: 'center',
         alignItems: "center",
-        backgroundColor: '#5DCA5C',
-        width: 200,
         height: 100,
         justifyContent: 'center',
         alignItems: "center",
-        backgroundColor: '#5DCA5C',
-        marginBottom: 10,
+        //  Check platform
+        backgroundColor: Platform.OS ? '#5DCA5C' : '#5DCA5C',
+        ...Platform.select({
+            ios: {
+                marginBottom: 10,
+            },
+            android: {
+                marginBottom: 10,
+            }
+        }),
+
         width: '50%',
     },
     btnText: {
