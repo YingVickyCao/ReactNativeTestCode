9e6f0f3d3f4d4ca365af26db679006bb02efd718
Vicky <ying.vicky.cao@outlook.com>
2019-08-26 16:41:27 +0800

    Update Counter.js

--- source/test/redux/reducers/Counter.js
+++ source/test/redux/reducers/Counter.js
@@ -10,16 +10,13 @@ export default (state = defaultState, action) => {
             return Object.assign({},state,{
                 num:state.num +1,
             })
-            break
 
         case TYPES.DECREMENT:
                 return Object.assign({},state,{
                     num:state.num -1,
                 })
-            break
 
         default:
             return state;
-            break;
     }
 }
