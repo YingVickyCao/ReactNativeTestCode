ee0786df987a527394ec16587397ac8904a33895
Vicky <ying.vicky.cao@outlook.com>
2019-09-04 10:54:05 +0800

    LodashStatic:_.tail(), _.head()

--- source/test_LoDashStatic/src/index.js
+++ source/test_LoDashStatic/src/index.js
@@ -12,8 +12,11 @@ function test() {
   // test_foreach([1, 2, 3]);
   // test_map([1, 2, 3]);
   // test_map_4_loadash([1, 2, 3]);
-  test_reduce();
+  // test_reduce();
   // test_get();
+  // test_chain();
+  // test_head();
+  test_tail();
 }
 
 /*
@@ -91,6 +94,66 @@ function test_get() {
       }
     }
   }
-  console.log(_.get(object2, 'a.b', '' )); // {c:3}
-  console.log(_.get(object2, 'a.b.c', '' )); // 3
+  console.log(_.get(object2, 'a.b', '')); // {c:3}
+  console.log(_.get(object2, 'a.b.c', '')); // 3
+}
+
+
+function test_chain() {
+  var users = [
+    { 'user': 'A', 'score': 10 },
+    { 'user': 'B', 'score': 100 },
+    { 'user': 'C', 'score': 90 }
+  ]
+  var minObj = _
+    .chain(users)
+    .sortBy('score')
+    .map(function (item) {
+      console.log(item.user + "'s score is " + item.score)
+    })
+    .head()
+    .value();
+
+  console.log("min=" + minObj);
+
+  var max = _
+    .chain(users)
+    .sortBy('score')
+    .map(function (item) {
+      console.log(item.user + "'s score is " + item.score)
+    })
+    .tail()
+    .value();
+
+  console.log("max=" + max);
+}
+
+function test_head() {
+  console.log(_.head([1, 2, 3]));
+  console.log(_.head([]));
+
+  var users = [
+    { 'user': 'A', 'score': 10 },
+    { 'user': 'B', 'score': 100 },
+    { 'user': 'C', 'score': 90 }
+  ]
+  var first = _.head(users);
+  console.log("first=" + JSON.stringify(first));
+
+  var last = _.tail(users);
+  console.log("last=" + JSON.stringify(last));
+}
+
+function test_tail() {
+  console.log(_.tail([1, 2, 3]));   // [2, 3]
+  console.log(_.tail([]));          // []
+
+  var users = [
+    { 'user': 'A', 'score': 10 },
+    { 'user': 'B', 'score': 100 },
+    { 'user': 'C', 'score': 90 }
+  ]
+
+  var tail = _.tail(users);
+  console.log("tail=" + JSON.stringify(tail));  // [{"user":"B","score":100},{"user":"C","score":90}]
 }
