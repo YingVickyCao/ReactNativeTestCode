575841619903db8b6c49e23bd7760c2dfadf5650
Vicky <ying.vicky.cao@outlook.com>
2019-09-09 21:01:06 +0800

    refactor

--- source/test/communication_between_components/parent_2_child/ParentView.js
+++ source/test/communication_between_components/parent_2_child/ParentView.js
@@ -9,41 +9,41 @@ import { styles } from '../CommunicationStyle';
 export default class ParentComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = { dateInfo: buildDate() }
-        this._onPress_4_update_date_info = this._onPress_4_update_date_info.bind(this);
+        this.state = { parent_to_child_1: buildDate() }
+        this._onPressUpdateDate = this._onPressUpdateDate.bind(this);
     }
 
-    _onPress_4_update_date_info() {
+    _onPressUpdateDate() {
         this.setState({
-            dateInfo: buildDate()
+            parent_to_child_1: buildDate()
         })
     }
 
-    componentDidMount() {
-        // 每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this._onPress_4_update_date_info();
-        }, 5000);
-    }
+    // componentDidMount() {
+    //     // 每1000毫秒对showText状态做一次取反操作
+    //     setInterval(() => {
+    //         this._onPress_4_update_date_info();
+    //     }, 5000);
+    // }
 
     render() {
         {/* 方式2： */ }
-        // const parent2Child = 'Parent -> Child'; 
+        // const parent_to_child_2 = 'Parent -> Child'; 
 
         {/* 方式3： */ }
-        let parent2Child = 'Parent -> Child';
+        let parent_to_child_2 = 'Parent -> Child';
 
         return (
             <View style={styles.container}>
                 <Text style={styles.topic}> Parent -> Child</Text>
                 <Text style={styles.label}> Parent</Text>
-                <Text style={styles.label}> {this.state.dateInfo}</Text>
-                <Text style={styles.btn} onPress={this._onPress_4_update_date_info}> Update </Text>
+                <Text style={styles.label}> {this.state.parent_to_child_1}</Text>
+                <Text style={styles.btn} onPress={this._onPressUpdateDate}> Update </Text>
 
                 {/* 写成变量的好处：向child传递的props 不能更改。定义成变量时，可以修改变量的值，从而自动改变child的显示  */}
-                <ChildComponent childData={parent2Child}></ChildComponent>
+                <ChildComponent childData={parent_to_child_2}></ChildComponent>
 
-                <ChildComponent style={{ marginTop: 20 }} childData={this.state.dateInfo}></ChildComponent>
+                <ChildComponent style={{ marginTop: 20 }} childData={this.state.parent_to_child_1}></ChildComponent>
 
                 {/* 方式1： */}
                 {/* <ChildComponent childData="ABC"></ChildComponent> */}
