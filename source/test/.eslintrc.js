5dffaaab85a9bf8cd3b2ec5ba8a477bcc17a5706
Vicky <ying.vicky.cao@outlook.com>
2019-09-23 16:30:44 +0800

    fix : console

--- source/test/.eslintrc.js
+++ source/test/.eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
 	plugins: ['prettier', 'react'],
 	extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
 	rules: {
-		'prettier/prettier': 'error'
+		'prettier/prettier': 'error',
+		'no-console': 'off'
+	},
+	env: {
+		node: true
 	},
 	parserOptions: {
 		ecmaVersion: 6,
--- source/test/src/book/TestLoginIn.js
+++ source/test/src/book/TestLoginIn.js
@@ -14,16 +14,16 @@ export default class LoginIn extends Component {
 		this._onChangeText_4_pwd = this._onChangeText_4_pwd.bind(this);
 	}
 
-	_onChangeText_4_phone(newValue) {
-		this.setState(state => {
-			return {
-				phone: newValue
-				/**
+	/**
          * 即使state 变量 的值不变，同样render(). 因此推测，React 框架在比较state时，比较的是state 变量的地址，而非值。
          * render(),  state:{phone: 123, pwd: ""}
            render(),  state:{phone: 123, pwd: ""}
          */
-				// phone: 123
+	// phone: 123
+	_onChangeText_4_phone(newValue) {
+		this.setState(state => {
+			return {
+				phone: newValue
 			};
 		});
 	}
