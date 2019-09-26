93fe50ac1cf785f8291f1df090eac5436c7f2743
Vicky <ying.vicky.cao@outlook.com>
2019-09-26 11:00:01 +0800

    Update TestColor.js

--- source/test/src/book/TestColor.js
+++ source/test/src/book/TestColor.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { styles } from '../tools/Styles';
-
+//  https://www.w3school.com.cn/cssref/css_colors_legal.asp
 export default class ColorComponent extends Component {
 	render() {
 		return (
