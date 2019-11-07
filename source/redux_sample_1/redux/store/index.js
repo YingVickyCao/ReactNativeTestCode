bf0e7e29d0d4afc46da16668578782f234f3be91
Vicky <ying.vicky.cao@outlook.com>
2019-11-07 09:46:13 +0800

    remove no use code

--- source/redux_sample_1/redux/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-import counter from '../reducers/Counter'
-
-const store = createStore(
-	counter
-)
-export default store;
